cc65dcf320636d9b2e8816a4a4827653a8a46d2f
nocaptara <antara4210@gmail.com>
2023-08-23 00:15:13 +0530

    alert added

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ while((guessmovie !=fav) && (guessmovie!="quit,i'm loser")){
     guess=prompt("ydk me,idiot");
 }
 if(guessmovie == fav){
-    console.log("hey darling, you guessed it right");
+    alert("hey darling, you guessed it right");
 }
 else{
-    console.log("bye,fuckoff");
+    alert("bye,fuckoff");
 }
